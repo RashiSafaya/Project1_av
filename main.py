a97b15a724a34ba27712143b97cf23d266d2cc78
RashiSafaya <rashisafaya@gmail.com>
2017-08-03 12:05:04 +0530

    spy identity.

---
+++ main.py
@@ -0,0 +1,6 @@
+print"Let's get started!!!"
+spy_name = raw_input("What is your name SPY: ")                      #defining variable.
+spy_salutation = raw_input("What should we call you(Mr. or Ms.): ")
+spy_name = spy_salutation +""+ spy_name                              #concatenation of name and salutation
+                                                                     #assignment operator parsing happens from r to l.
+print "WELCOME " + spy_name + " Glad to have you back with us!"
