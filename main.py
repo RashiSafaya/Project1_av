9bc8b9da6e331ab5c1270c11d1e0a8bcafd201e5
RashiSafaya <rashisafaya@gmail.com>
2017-08-03 12:29:41 +0530

    conditional statement

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 print"Let's get started!!!"
-spy_name = raw_input("What is your name SPY: ")                      #defining variable.
+spy_name = raw_input("What is your name SPY: ")                      # defining variable.
 spy_salutation = raw_input("What should we call you(Mr. or Ms.): ")
-spy_name = spy_salutation +""+ spy_name                              #concatenation of name and salutation
-                                                                     #assignment operator parsing happens from r to l.
-print "WELCOME " + spy_name + " Glad to have you back with us!"
+
+if len(spy_name)>0:
+
+spy_name = spy_salutation +""+ spy_name                              # concatenation of name and salutation
+print "WELCOME " + spy_name + " Glad to have you back with us!"      # assignment operator parsing happens from r to l.
+
+                                                                     #name = "Mr.Bond"
+                                                                     #if len(name)>0:
+                                                                         #new_message = "YAY! Name is not empty"
+                                                                         #print new_message
