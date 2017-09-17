59b6866235c0a4f51e150eddcdc1751ae7d54ccf
RashiSafaya <30629601+RashiSafaya@users.noreply.github.com>
2017-09-17 23:11:01 +0530

    Update read_chat.py

--- read_chat.py
+++ read_chat.py
@@ -1,6 +1,6 @@
 from colorama import Fore
-from globals import friends,spy
-from select_friend import select_friend
+from globals import friends, spy
+from selectfriend import select_friend
 
 
 def read_chat():
@@ -8,10 +8,12 @@ def read_chat():
     for chat in friends[friend_choice].chats:
         if chat.sent_by_me:
             # displaying chats with different colour codes
-            print "[%s] %s: \n%s (no of words:%s)" % (Fore.BLUE+" "+chat.time.strftime("%d %B %Y"),
-                                                      Fore.RED+" "+spy.getName(), Fore.BLACK+" "+chat.message,
-                                                      " "+str(chat.no_of_words))
+            print "[%s] %s: \n%s (no of words:%s)" % (Fore.BLUE + " " + chat.time.strftime("%d %B %Y"),
+                                                      Fore.RED + " " + spy.getName(), Fore.BLACK + " " + chat.message,
+                                                      " " + str(chat.no_of_words))
         else:
             print "[%s] %s: \n%s (no of words:%s)" % [Fore.BLUE + ' ' + chat.time.strftime("%d %B %Y"),
-                                                      Fore.RED+" " + friends[friend_choice].name, Fore.BLACK+" " + chat.message,
+                                                      Fore.RED + " " + friends[friend_choice].name,
+                                                      Fore.BLACK + " " + chat.message,
                                                       " " + str(chat.no_of_words)]
+
