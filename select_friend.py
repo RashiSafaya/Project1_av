eae654c74879ce36c4094ba618048aff06d00e97
RashiSafaya <30629601+RashiSafaya@users.noreply.github.com>
2017-09-17 23:11:58 +0530

    Update select_friend.py

--- select_friend.py
+++ select_friend.py
@@ -1,9 +1,10 @@
 from globals import friends
 
+
 def select_friend():
-    counter = 1
+    counter = 0
     for friend in friends:
-        print str(counter) + ". " + friend.getName()
+        print "%d." % (counter+1)+ friend.getName()
         counter = counter + 1
 
     result = int(raw_input("Select from the list : "))
