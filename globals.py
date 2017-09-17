c5b33cd3e21a1ee636d1c74a4d6c432bcf5d173f
RashiSafaya <30629601+RashiSafaya@users.noreply.github.com>
2017-09-17 23:09:32 +0530

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,9 +1,8 @@
 # global variables and constants.
-from spy_details import spy
+from spydetails import spy
+
 spy = spy()
-
-# list to store status messages.
-status_messages = ["Spy mode ON", "No message, only call"]
-
 # lists to store users friends information.
 friends = []
+
+
