a3ecbaf89df38cf613d864c0a338b2a2afafc416
RashiSafaya <30629601+RashiSafaya@users.noreply.github.com>
2017-09-17 23:08:18 +0530

    Update add_friend.py

--- add_friend.py
+++ add_friend.py
@@ -1,20 +1,21 @@
 # import statements.
-from globals import friends,spy
-from spy_details import spy
+from globals import friends, spy
+from spydetails import spy
+
 
 # add new friends.
 def add_friend():
     new_friend = spy()
     print "Enter your friend's details \n"
-
     new_friend.name = raw_input("Enter the name of your friend: ")
     new_friend.salutation = raw_input("Are they Mr. or Ms.? ")
-    new_friend.name = new_friend.name + " " + new_friend.salutation             # concatenation.
+    new_friend.name = new_friend.name + " " + new_friend.salutation  # concatenation.
     new_friend.age = int(raw_input("Age?"))
     new_friend.rating = float(raw_input("Spy rating?"))
 
     # validating input
     if len(new_friend.name) > 0 and 12 < new_friend.age < 60:
+        print " "+str(spy.rating)
         if new_friend.rating >= spy.rating:
             # add_friend
             friends.append(new_friend)
