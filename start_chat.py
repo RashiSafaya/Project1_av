5740ece27c6a8259565036aa52ec34166744cd4f
RashiSafaya <30629601+RashiSafaya@users.noreply.github.com>
2017-09-17 23:13:28 +0530

    Update start_chat.py

--- start_chat.py
+++ start_chat.py
@@ -1,49 +1,18 @@
 # import statements.
 
+
 import re
 from globals import spy
-from add_friend import add_friend
-from add_status import add_status
-from read_message import read_message
-from send_message import send_message
-from read_chat import read_chat
+from addfriend import add_friend
+from addstatus import add_status
+from readmessage import read_message
+from sendmessage import send_message
+from readchat import read_chat
+
 
 # start_chat() function definition
 def start_chat():
-
-    # validating users details.
-    error_message = None  # variable for storing error messages.
-    pattern = '^[0-9]+$'
-    if re.match(pattern, str(spy.age) is None) and not (12 < spy.age < 50):
-            # invalid age.
-        error_message = "Invalid age. Provide correct details."
-        print error_message
-    else:
-        spy.rating = float(raw_input("What is your spy rating?"))         # typecasting(int to float)
-        pattern = '^[0-9]+\.[0-9]+$'
-        if re.match(pattern, str(spy.rating) is not None):
-            rating = spy.rating
-            if spy.rating > 4.7:
-                print "Great Work Spy!"
-            elif 3.5 < spy.rating <= 4.7:
-                print "You are doing amazing Spy!"
-            elif 2 < spy.rating <= 3.5:
-                print "Can do much better Spy!"
-            else:
-                print "You need to buckle up Spy!" \
-
-            spy_is_online = True
-            welcome_message = "Authentication complete. Welcome\n " \
-                              "Name : " + spy.name + "\n" \
-                              "Age: " + str(spy.age) + "\n" \
-                              "Rating: " + str(spy.rating) + "\n" \
-                              "You are awesome \n "
-
-
-            print welcome_message
-
-            # displaying menu for user.
-
+      # displaying menu for user.
             show_menu = True
             while show_menu:
                 menu_choices = "What do you want to do ? \n " \
@@ -56,12 +25,12 @@ def start_chat():
 
                 result = int(raw_input(menu_choices))
 
-            # validating users input
+                # validating users input
                 if result == 1:
                     current_status_message = spy.current_status_message
-                    print "Your Current status is: " +str(current_status_message)
+                    print "Your Current status is: " + str(current_status_message)
                     spy.current_status_message = add_status()
-                    print "Your new status is: " +str(spy.current_status_message)
+                    print "Your new status is: " + str(spy.current_status_message)
                 elif result == 2:
                     no_of_friends = add_friend()
                     print "You have %d friends " % no_of_friends
@@ -75,7 +44,3 @@ def start_chat():
                     show_menu = False
                 else:
                     print "Wrong choice. Please try again."
-        else:
-            print "Invalid value. Please try again with a valid value."
-
-
