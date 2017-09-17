05b9fd4b15c1ef55bcc639825e982689fe60ae08
RashiSafaya <30629601+RashiSafaya@users.noreply.github.com>
2017-09-17 23:14:03 +0530

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -1,7 +1,7 @@
-
 import re
+
 
 def word_count(string):
     while True:
-        count = len(re.findall("[a-zA-Z_]+", string))
+     count = len(re.findall("[a-zA-Z_]+", string))
     return count
